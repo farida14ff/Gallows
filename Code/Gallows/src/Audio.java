96ad1052f18aad6c2aedb1b228a77af0c9cacf61
farida14ff <45116829+farida14ff@users.noreply.github.com>
2019-05-19 13:25:06 +0600

    Update Audio.java

--- Code/Gallows/src/Audio.java
+++ Code/Gallows/src/Audio.java
@@ -8,10 +8,12 @@ public class Audio {
     private Clip clip = null;
     private FloatControl volumeC = null;
     private double wt;
+    private boolean background;
 
     public Audio (String track, double wt){
         this.track = track;
         this.wt = wt;
+        this.background = false;
     }
 
     public void sound(){
@@ -38,11 +40,50 @@ public class Audio {
             e.printStackTrace();
         }
     }
+    public void play(){
+        File f = new File(this.track);
+        AudioInputStream tr = null;
+        try {
+            tr = AudioSystem.getAudioInputStream(f);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            clip = AudioSystem.getClip();
+            clip.open(tr);
+            volumeC = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+
+            if (!this.background) {
+                clip.setFramePosition(0);
+                clip.start();
+                this.background = true;
+            }
+
+        } catch (LineUnavailableException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop(){
+        clip.stop();
+        clip.close();
+        this.background = false;
+
+    }
     public void setVolume(){
         if(wt<0) wt = 0;
         if(wt>1) wt = 1;
         float min = volumeC.getMinimum();
         float max = volumeC.getMaximum();
         volumeC.setValue(max-min * (float)wt + min);
+    }
+    public void repeat() {
+        if (this.background)
+            clip.loop(100);
     }
 }
